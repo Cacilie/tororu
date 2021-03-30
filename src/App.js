19cb14930546358f061957a3c48248612a916171
Cacilie <hi.ed@hotmail.com>
2021-03-29 23:53:39 -0600

    Splashscreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Link } from "react-router-dom"; 
 import Home from './Components/HomeComponent';
+import About from './Components/AboutComponent';
+
 class App extends Component  {
 
 	render(){
 		return (
 		    <HashRouter basename="/" className="fill-window">
                 <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+
 		    </HashRouter>
 
 		)
