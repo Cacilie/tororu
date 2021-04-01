31cf0d233750f011c77b3d8c957230c91b3a77c9
Cacilie <hi.ed@hotmail.com>
2021-04-01 11:45:00 -0600

    Drawer added but inactive

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './HomeComponent.css'
+import { Drawer, ButtonToolbar, Button } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
+
 
 export default function Home() {
 
     const [value, setValue] = useState('');
     const [rows, setRows] = useState(5);
-    const [minRows, setMinRows] = useState(5);
-    const [maxRows, setMaxRows] = useState(100000);
+    const maxRows = 1000;
 
 
     function handleChange(event) {
@@ -27,19 +29,21 @@ export default function Home() {
 
         setValue(event.target.value)
         localStorage.setItem("@tororu/value", event.target.value)
-        setRows(rows > currentRows ? rows : currentRows < maxRows ? currentRows + 200 : maxRows)
-        localStorage.setItem("@tororu/rows", rows > currentRows ? rows : currentRows < maxRows ? currentRows + 200 : maxRows)
-        
+        setRows(rows > currentRows ? rows : currentRows < maxRows ? currentRows  : maxRows)
+        localStorage.setItem("@tororu/rows", rows > currentRows ? rows : currentRows < maxRows ? currentRows  : maxRows)
+
     };
-    useEffect( () => {
+    useEffect(() => {
         let tororu_value = localStorage.getItem("@tororu/value")
         let tororu_rows = localStorage.getItem("@tororu/rows")
         setValue(tororu_value)
         setRows(tororu_rows)
 
-    } , [])
+    }, [])
 
     return (
+        <div>
+            <Menu />
             <textarea
                 rows={rows}
                 value={value}
@@ -47,7 +51,35 @@ export default function Home() {
                 className={'textarea'}
                 onChange={handleChange}
             />
+            
+        </div>
+
     );
 }
 
 
+function Menu() {
+
+    const [show, setShow] = useState(false)
+
+
+    return (
+        <div>
+            <Drawer
+                size={'xs'}
+                show={show}
+                onHide={() => setShow(false)}
+            >
+                <Drawer.Header>
+                    <Drawer.Title>Drawer Title</Drawer.Title>
+                </Drawer.Header>
+                <Drawer.Body>
+                </Drawer.Body>
+                <Drawer.Footer>
+                    <Button onClick={() => setShow(false)} appearance="primary">Confirm</Button>
+                    <Button onClick={() => setShow(false)} appearance="subtle">Cancel</Button>
+                </Drawer.Footer>
+            </Drawer>
+        </div>
+    )
+}
