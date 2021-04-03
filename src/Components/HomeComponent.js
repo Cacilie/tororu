48cd7658c67a87326c5eb1306ba6fe2ca8564387
Cacilie <hi.ed@hotmail.com>
2021-04-03 00:32:05 -0600

    Erase stories functionaliry added

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './HomeComponent.css'
-import { Drawer, IconButton, Icon } from 'rsuite';
+import { Drawer, IconButton, Icon, List } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-dark.css';
 import styled from '@emotion/styled'
 import { Collection, Item } from 'japidb';
 
-const stories = new Collection('Stories', 'id');
+const storiesApi = new Collection('Stories', 'id');
 const currentStoryId = new Item('currentStoryId', 0);
 
 export default function Home() {
     const [show, setShow] = useState(false)
     const [value, setValue] = useState('');
     const [rows, setRows] = useState(5);
+    const [stories, setStories] = useState([])
     const maxRows = 1000;
 
 
@@ -33,41 +34,41 @@ export default function Home() {
 
         const finalRows = rows > currentRows ? rows : currentRows < maxRows ? currentRows : maxRows
 
-        if(currentStoryId.get() === 0){
+        if (currentStoryId.get() === 0) {
             const id = Math.floor(Date.now() / 1000)
 
-            stories.save({
-                "id" : id,
-                "story" : event.target.value,
-                "rows" : finalRows
+            storiesApi.save({
+                "id": id,
+                "story": event.target.value,
+                "rows": finalRows
             });
 
             currentStoryId.save(id)
-        }else {
-            stories.save({
+        } else {
+            storiesApi.save({
                 "id": currentStoryId.get(),
                 "story": event.target.value,
                 "rows": finalRows
             })
         }
-     
+
         setValue(event.target.value)
         setRows(finalRows)
+        setStories(storiesApi.find())
+
 
     };
     useEffect(() => {
-        
-
-        let tororu_value = localStorage.getItem("@tororu/value")
-        let tororu_rows = localStorage.getItem("@tororu/rows")
 
         //this code snippet is to save work from previous versions
-        if(tororu_value){
+        let tororu_value = localStorage.getItem("@tororu/value")
+        let tororu_rows = localStorage.getItem("@tororu/rows")
+        if (tororu_value) {
             const id = Math.floor(Date.now() / 1000)
-            stories.save({
-                "id" : id,
-                "story" : tororu_value,
-                "rows" : tororu_rows
+            storiesApi.save({
+                "id": id,
+                "story": tororu_value,
+                "rows": tororu_rows
             })
 
             currentStoryId.save(id)
@@ -76,14 +77,35 @@ export default function Home() {
         }
         // end of code snippet
 
-        let firstStory = stories.find({
-            "id" : currentStoryId.get()
-        })
-        if(firstStory.length > 0 ) firstStory = firstStory[0]        
+        let displayStory = storiesApi.get(currentStoryId.get())
 
-        setValue(firstStory.story)
-        setRows(firstStory.rows)
+        setValue(displayStory ? displayStory.story : '')
+        setRows(displayStory ? displayStory.rows : 5)
+
+        setStories(storiesApi.find())
     }, [])
+
+    const setCurrentStory = (id) => {
+        currentStoryId.save(id);
+        let displayStory = storiesApi.get(currentStoryId.get())
+        let story = displayStory ? displayStory.story : ''
+        setValue(story)
+        let rows = displayStory ? displayStory.rows : 5
+        setRows(rows)
+        setStories(storiesApi.find())
+    }
+
+    const removeStory = (id) => {
+        if(id === currentStoryId.get()) startNewStory()
+        storiesApi.remove(id)
+        setStories(storiesApi.find())
+    }
+
+    const startNewStory = () => {
+        currentStoryId.save(0);
+        setValue('')
+        setRows(5)
+    }
 
     const MenuToolbar = styled('ButtonToolbar')`
       position: absolute;
@@ -96,13 +118,13 @@ export default function Home() {
 
     return (
         <div>
-            <Menu show={show} setShow={setShow} />
+            <Menu show={show} setShow={setShow} setCurrentStory={setCurrentStory} stories={stories} startNewStory={startNewStory} removeStory={removeStory} />
             <MenuToolbar>
-                <IconButton icon={<Icon icon="align-justify" className={'menuIcons'} />}  appearance={'link'} onClick={() => setShow(true)} />
+                <IconButton icon={<Icon icon="align-justify" className={'menuIcons'} />} appearance={'link'} onClick={() => setShow(true)} />
             </MenuToolbar>
             <textarea
                 rows={rows}
-                defaultValue={value}
+                value={value}
                 placeholder={'Write!'}
                 className={'textarea'}
                 onChange={handleChange}
@@ -114,7 +136,7 @@ export default function Home() {
 }
 
 
-function Menu({ show, setShow }) {
+function Menu({ show, setShow, setCurrentStory, stories, startNewStory, removeStory }) {
 
 
 
@@ -130,9 +152,20 @@ function Menu({ show, setShow }) {
                     <Drawer.Title>トロール (Tororu)</Drawer.Title>
                 </Drawer.Header>
                 <Drawer.Body>
+                    <List bordered>
+                        {stories.map((story, index) => {
+                            return (
+                                <List.Item key={story.id} index={index} >
+                                    {story.story.slice(0,35)}
+                                    <IconButton icon={<Icon icon="right" className={'menuIcons'} />} onClick={() => setCurrentStory(story.id)} appearance="subtle" />
+                                    <IconButton icon={<Icon icon="trash" className={'menuIcons'} />} onClick={() => removeStory(story.id)} appearance="subtle" />
+                                </List.Item>
+                            )
+                        })}
+                    </List>
                 </Drawer.Body>
                 <Drawer.Footer>
-                <IconButton icon={<Icon icon="plus" className={'menuIcons'} />}  appearance={'link'} onClick={() => setShow(true)} />
+                    <IconButton icon={<Icon icon="plus" className={'menuIcons'} />} appearance={'link'} onClick={() => startNewStory()} />
 
                 </Drawer.Footer>
             </Drawer>
